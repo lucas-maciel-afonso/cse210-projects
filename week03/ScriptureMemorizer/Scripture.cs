d1f5b378e5525bcaba850d742b82ee536951034a
lucas-maciel-afonso <lafonso@byupathway.edu>
2025-02-08 11:00:00 -0700

    W03 Project: Scripture Memorizer Program

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,34 @@
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < numberToHide && visibleWords.Count > 0; i++)
+        {
+            Word wordToHide = visibleWords[rand.Next(visibleWords.Count)];
+            wordToHide.Hide();
+            visibleWords.Remove(wordToHide);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        return _reference.GetDisplayText() + "\n" + string.Join(" ", _words.Select(w => w.GetDisplayText()));
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
