25e60045e468d84dcb4985684badcf9109b8da96
lucas-maciel-afonso <lafonso@byupathway.edu>
2025-02-20 09:12:39 -0700

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Maria Andrade", "Algebra");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Jonas Winter", "Exponents", "1.5", "10-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Peter Walls", "Economy of Brazil", "The Period of Colonization");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
